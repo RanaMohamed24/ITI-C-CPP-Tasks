3a70849bb5b2b4535f02bac8f7fda07afed3818b
RanaMohamed24 <raanaamohamed695@gmail.com>
2025-12-09 18:46:02 +0200

    pointer to pointer

--- task_11/relation_task/picture.cpp
+++ task_11/relation_task/picture.cpp
@@ -1,66 +1,30 @@
-#include "picture.h"
+#include "picture.h";
 using namespace std;
 
 Picture::Picture() {
-    cNum = rNum = lNum = 0;
-    pC = nullptr;
-    pR = nullptr;
-    pL = nullptr;
-   
+    shapeNum = 0;
+    pShapes = nullptr;
 }
 
 Picture::~Picture() {
    
 }
 
-void Picture::setCircles(int cn, Circle* cArr) {
-    cNum = cn;
-    pC = cArr;
-}
-
-void Picture::setRects(int rn, Rect* rArr) {
-    rNum = rn;
-    pR = rArr;
-}
-
-void Picture::setLines(int ln, Line* lArr) {
-    lNum = ln;
-    pL = lArr;
+void Picture::setShapes(int sn, Shape** sArr) {
+    shapeNum = sn;
+    pShapes = sArr;
 }
 
 void Picture::draw(SDL_Renderer* renderer) {
-    for(int i = 0; i < cNum; i++) {
-        pC[i].draw(renderer);
-    }
-    for(int i = 0; i < rNum; i++) {
-        pR[i].draw(renderer);
-    }
-    
-    for(int i = 0; i < lNum; i++) {
-        pL[i].draw(renderer);
+    for(int i = 0; i < shapeNum; i++) {
+        pShapes[i]->draw(renderer);
     }
 }
 
 void Picture::print() {
-   
-    
-    cout << "\nCircles (" << cNum << "):" << endl;
-    for(int i = 0; i < cNum; i++) {
+    cout << "\nShapes (" << shapeNum << "):" << endl;
+    for(int i = 0; i < shapeNum; i++) {
         cout << "  ";
-        pC[i].print();
+        pShapes[i]->print();
     }
-    
-    cout << "\nRectangles (" << rNum << "):" << endl;
-    for(int i = 0; i < rNum; i++) {
-        cout << "  ";
-        pR[i].print();
-    }
-    
-    cout << "\nLines (" << lNum << "):" << endl;
-    for(int i = 0; i < lNum; i++) {
-        cout << "  ";
-        pL[i].print();
-    }
-    
-   
 }
--- task_11/relation_task/picture.h
+++ task_11/relation_task/picture.h
@@ -1,26 +1,19 @@
 #ifndef PICTURE_H
 #define PICTURE_H
 
-#include "./circle.h"
-#include "./rect.h"
-#include "./line.h"
+#include <iostream>
+#include "./shape.h"
 
 class Picture {
 private:
-    int cNum;
-    int rNum;
-    int lNum;
-    Circle* pC;
-    Rect* pR;
-    Line* pL;
+    int shapeNum;
+    Shape** pShapes;
     
 public:
     Picture();
     ~Picture();
     
-    void setCircles(int cn, Circle* cArr);
-    void setRects(int rn, Rect* rArr);
-    void setLines(int ln, Line* lArr);
+    void setShapes(int sn, Shape** sArr);
     void draw(SDL_Renderer* renderer);
     void print();
 };
